ea68aee64f70a3b9421d56b47a9d4a4d6c2aa9a2
Thomas F <taforres@asu.edu>
2016-06-12 17:44:46 -0700

    oops

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    message = client.messages.create(to="4807603293",from_="+15005550006",
+    message = client.messages.create(to="",from_="",
     body="Test API")
     return message.body
     
