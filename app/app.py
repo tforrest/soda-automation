46e5d5bfe3eae4964d972b80874e64df95e03c5f
Thomas F <taforres@asu.edu>
2016-06-14 18:43:26 -0700

    Remove git stuff from app.py

--- app/app.py
+++ app/app.py
@@ -2,16 +2,9 @@ from flask import Flask
 from twilio_client import client
 # config test app
 app = Flask(__name__)
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/thomas/PersonalProjects/soda-twilio-service/test.db'
 db = SQLAlchemy(app)
-=======
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
->>>>>>> parent of 92d6afe... Add Flask-SQLAlchemy
-=======
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
->>>>>>> parent of 92d6afe... Add Flask-SQLAlchemy
 
 
 @app.route('/',methods=['GET','POST'])
