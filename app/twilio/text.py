def861971f16360e3ed761ec2af0861c5b6f1558
Thomas F <taforres@asu.edu>
2016-06-14 18:44:34 -0700

    Add initial twilio wrapper with basic error handling

---
+++ app/twilio/text.py
@@ -0,0 +1,48 @@
+from app import app
+from twilio import TwilioRestClient
+
+class TwilioWrapper(object):
+    
+    def __init__(self,signature,debug):
+        self._twilio_client = _get_twilio_client()
+        self._twilio_number = app.config['TWILIO_NUMBER']
+        self._signature = signature
+        self.debug = debug
+        
+    def _get_twilio_client(self):
+        
+        try:
+            twilio_account_sid =  app.config['TWILIO_ACCOUNT_SID']
+            twilio_auth_token = app.config['TWILIO_AUTH_TOKEN']
+            client = TwilioRestClient(account_sid,auth_token)
+            return client
+        except:
+            raise TwilioWrapperException("Failure to generate client")
+    
+    def send_single_message(self,to,from,message):
+        
+        message = self._twilio_client.messages.create(
+            to=to,
+            from_=from,
+            body=message,
+        )
+        
+        return _hand_twilio_text_respose(message.response)
+        
+    def send_messages(self,from,message,recipients):
+        
+        for recipient in recipients:
+            message = self._twilio_client.messages.create(
+                to=recipient.to
+                from_=from,
+                body = message
+            )
+            if not self._handl_twilio_text_response(message.response):
+                return False
+       return True
+        
+    def _handle_twilio_text_response(self,response):
+        return True 
+
+class TwilioWrapperException(Exception):
+    pass
--- app/twilio_client.py
+++
@@ -1,7 +0,0 @@
-from twilio.rest import TwilioRestClient 
-
-# Test api info
-account_sid = ""
-auth_token = ""
-
-client = TwilioRestClient(account_sid,auth_token)
