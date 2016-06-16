6241d532a379118342b8641a4815fcb7de3d3a84
Thomas F <taforres@asu.edu>
2016-06-15 19:13:43 -0700

    Revert "Add to .gitignore"
    This reverts commit bcc9d1d89e163dc320d2234de702567923b19c52.

--- app/app.py
+++ app/app.py
@@ -1,13 +1,17 @@
-from config import app
-from messaging import text
-from flask import make_response
+from flask import Flask 
+from twilio_client import client
+# config test app
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/thomas/PersonalProjects/soda-twilio-service/test.db'
+db = SQLAlchemy(app)
 
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    T = text.TwilioTextWrapper("test",True)
-    print "test"
-    x = T.send_message("15005550006","test")
-    return make_response(x.body)
+    message = client.messages.create(to="",from_="",
+    body="Test API")
+    return message.body
+    
 if __name__ == '__main__':
     app.run(debug=True)
