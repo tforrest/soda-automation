8825b481243ba5bc50adef73e81ed3753519a417
Thomas F <taforres@asu.edu>
2016-06-28 23:00:54 -0700

    Add parser for single user in post to update list

--- app/api/api.py
+++ app/api/api.py
@@ -8,16 +8,28 @@ list_info = {
     'list_name': fields.String
 }
 
+student_field = {
+    'first_name' :fields.String,
+    'last_name' : fields.String,
+    'email': fields.String,
+    'number': fields.String,
+    'asuid' : fields.String,
+    'class_standing': fields.String
+}
+
 class MailChimpApi(Resource):
     """Rest API for parsing MailChimp Data"""
  
     def get(self,list_id):
         """A list of members from mailchimp"""
         args = self.parse_list_id().parse_args()
-        
+        r = chimp.ChimpRequester()
+        return r.get_list(list_id,"test",json=True)
+    @marshal_with(student_field)  
+    def post(self,list_id):
+        args = self.parse_list_id().parse_args()
         r = chimp.ChimpRequester()
         
-        return r.get_list(list_id,"test",json=True)
         
         
     def parse_list_id(self):
@@ -27,6 +39,32 @@ class MailChimpApi(Resource):
             type=str, help='ID of the mailchimp list',
         )
         return lid_parser
+        
+    def parse_incoming_student(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            'first_name', dest='fiest_name',
+            type=str, help='First name of student',
+            required=True,
+        )
+        parser.add_argument(
+            'last_name', dest='last_name',
+            type=str, help='Last name of student',
+            required=True,
+        )
+        parser.add_argument(
+            'email', dest='email',
+            type=str, help='Email of student',
+        )
+        parser.add_argument(
+            'number', dest='number',
+            type=str, help="Cell Number for student",
+        )
+        parser.add_argument(
+            'asuid', dest='asuid',
+            type=str, required=True,
+            help='ASU id of student',
+        )
         
         
         
@@ -41,11 +79,3 @@ list_info = {
 #     'students' : fields.Nested(student_field)
 # }   
    
-# student_field = {
-#     'first_name' :fields.String,
-#     'last_name' : fields.String,
-#     'email': fields.String,
-#     'number': fields.String,
-#     'asuid' : fields.String,
-#     'class_standing': fields.String
-# }
