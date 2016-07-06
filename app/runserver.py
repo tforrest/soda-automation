d913b0a7f0031ca96ebe8f653f232a151cfca0bf
Thomas F <taforres@asu.edu>
2016-07-05 21:41:05 -0700

    Add flask-script package to manage deploys

--- app/runserver.py
+++ app/runserver.py
@@ -1,14 +1,16 @@
 from flask_script import Manager
-
 from config.config import app
-# from mailchimp.redis_init import init_redis_with_mailchimp
 
 manager = Manager(app)
 
 @manager.command
 def run_local():
-    #init_redis_with_mailchimp(redis_server)
     app.run(debug=True)
+
+@manager.command
+def run_test():
+    # To-Do
+    pass
 
 @manager.command
 def run_production():
