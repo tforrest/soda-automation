a8bd42f3d3b0e8a11c55cfe9b531617f7ed9f931
Thomas F <taforres@asu.edu>
2016-07-07 19:23:46 -0700

    Create basic user model for token creation

---
+++ app/models/user.py
@@ -0,0 +1,21 @@
+from config import db
+from passlib.apps import custom_app_context as pwd_context
+
+import os
+
+class User(db.Model):
+    """Basic user model for token creation"""
+    id = db.Column(db.Integer,primary_key=True)
+    user_name = db.Column(db.String(60), index=True)
+    password = db.Column(db.String(60))
+    # dont expose through the API!
+    def hash_pass(self,pwd):
+        """Hash Password to store"""
+        self.password = pwd_context.encrypt(pwd)
+    
+    def check_pass(self,pwd):
+        """Checks if the password is correct"""
+        return pwd_context.verify(pwd, self.password)
+    
+        
+
